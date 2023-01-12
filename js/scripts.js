9e3043fca9263a8bf679eb431b9f0f3c61be7456
Marco <118975127+marcoHildebrandt@users.noreply.github.com>
2023-01-12 16:27:05 +0100

    pokemons added

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-alert('Hello world');
-
-let favoriteFood = "Pizza";
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: "Kangaskhan",
+        height: 2.2,
+        types: "Normal"
+    },
+    {
+        name: "Snorlax",
+        height: 2.1,
+        types: "Normal"
+    },
+    {
+        name: "Lapras",
+        height: 2.5,
+        types: ["Ice", "Water"]
+    },
+    {
+        name: "Aron",
+        height: 0.4,
+        types: ["Steel", "Rock"]
+    },
+    {
+        name: "Poochyena",
+        height: 0.5,
+        types: "Dark"
+    },
+];
