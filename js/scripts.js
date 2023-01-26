09ca3d9edcb11cf5fb0303591a64f165d43ca2fd
Marco <118975127+marcoHildebrandt@users.noreply.github.com>
2023-01-26 18:09:47 +0100

    wrapped array in an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,44 @@
-let pokemonList = [
-    {
-        name: "Kangaskhan",
-        height: 2.2,
-        types: "Normal"
-    },
-    {
-        name: "Snorlax",
-        height: 2.1,
-        types: "Normal"
-    },
-    {
-        name: "Lapras",
-        height: 2.5,
-        types: ["Ice", "Water"]
-    },
-    {
-        name: "Aron",
-        height: 0.4,
-        types: ["Steel", "Rock"]
-    },
-    {
-        name: "Poochyena",
-        height: 0.5,
-        types: "Dark"
-    },
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: "Kangaskhan",
+            height: 2.2,
+            types: "Normal"
+        },
+        {
+            name: "Snorlax",
+            height: 2.1,
+            types: "Normal"
+        },
+        {
+            name: "Lapras",
+            height: 2.5,
+            types: ["Ice", "Water"]
+        },
+        {
+            name: "Aron",
+            height: 0.4,
+            types: ["Steel", "Rock"]
+        },
+        {
+            name: "Poochyena",
+            height: 0.5,
+            types: "Dark"
+        },
+    ];
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
 
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
 //for loop
 // for (let i = 0; i < pokemonList.length; i++) {
 //     document.write("<p>" + "<h1>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ") " + "</h1>" + "</p>");
@@ -37,7 +50,7 @@ let pokemonList = [
 // }
 
 //forEach loop
-pokemonList.forEach(function (pokemon) {
+pokemonRepository.getAll().forEach(function (pokemon) {
     document.write("<p>" + "<h1>" + pokemon.name + " (height: " + pokemon.height + ") " + "</h1>" + "</p>");
     if (pokemon.height >= 2.0 && pokemon.height <= 2.4) {
         document.write(" - Wow, that's big! ");
@@ -45,3 +58,4 @@ pokemonList.forEach(function (pokemon) {
         document.write(" - Wow, that's really big! ")
     }
 });
+
