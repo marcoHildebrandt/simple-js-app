32c77a53094a1d21576d386fa1a1357ec23f18bf
Marco <118975127+marcoHildebrandt@users.noreply.github.com>
2023-02-02 15:57:02 +0100

    created an addListItem()

--- js/scripts.js
+++ js/scripts.js
@@ -34,9 +34,20 @@ let pokemonRepository = (function () {
         return pokemonList;
     }
 
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listItem = document.createElement("li");
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listItem.appendChild(button);
+        pokemonList.appendChild(listItem);
+    }
+
     return {
         add: add,
-        getAll: getAll
+        getAll: getAll,
+        addListItem: addListItem
     };
 })();
 //for loop
@@ -51,11 +62,12 @@ let pokemonRepository = (function () {
 
 //forEach loop
 pokemonRepository.getAll().forEach(function (pokemon) {
-    document.write("<p>" + "<h1>" + pokemon.name + " (height: " + pokemon.height + ") " + "</h1>" + "</p>");
-    if (pokemon.height >= 2.0 && pokemon.height <= 2.4) {
-        document.write(" - Wow, that's big! ");
-    } else if (pokemon.height >= 2.5) {
-        document.write(" - Wow, that's really big! ")
-    }
+    pokemonRepository.addListItem(pokemon);
+    // document.write("<p>" + "<h1>" + pokemon.name + " (height: " + pokemon.height + ") " + "</h1>" + "</p>");
+    // if (pokemon.height >= 2.0 && pokemon.height <= 2.4) {
+    //     document.write(" - Wow, that's big! ");
+    // } else if (pokemon.height >= 2.5) {
+    //     document.write(" - Wow, that's really big! ")
+    // }
 });
 
